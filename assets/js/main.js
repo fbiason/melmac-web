8e7c764f1afc1dabd082340114694b0f40e8f514
fbiason <franco.biason@gmail.com>
2022-08-04 21:12:36 -0300

    Desafio DOM

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,20 +1,24 @@
 //varlable
 let tipoComida = ["Hamburguesa", "Pizza", "Empanadas", "Milanesa"];
-let mezcla = [
-  "Papas Fritas con <br>",
-  "Salchichas con <br>",
-  "Tomate con <br>",
-  "Sopa con <br>",
-  "Postre con <br>",
-  "Pescado con <br>",
+let bebida = [
+  "Agua con <br>",
+  "Cerveza con <br>",
+  "Vino con <br>",
+  "Leche con <br>",
+  "Mate con <br>",
+  "Café con <br>",
+  "Té con <br>",
+  "Fernet con <br>",
+  "Coca con <br>",
 ];
 
 function displayCard() {
-  let comidaMezclada =
+  let bebidaConComida =
     tipoComida[Math.floor(Math.random() * tipoComida.length)];
-  let masMezcla = mezcla[Math.floor(Math.random() * mezcla.length)];
-  let verComida = `${masMezcla}   ${comidaMezclada}`;
+  let verBebida = bebida[Math.floor(Math.random() * bebida.length)];
+  let verComida = `${verBebida}   ${bebidaConComida}`;
   document.getElementById("cualquierMenu").style.background = "yellow";
   document.getElementById("cualquierMenu").style.textAlign = "center";
   document.getElementById("cualquierMenu").innerHTML = verComida;
+  document.getElementById("will").src = "assets/img/will.png";
 }
